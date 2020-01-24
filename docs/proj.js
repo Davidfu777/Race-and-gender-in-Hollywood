c8680d8a18a337f877dd9c2d154347c609fcee06
Enoch Mwesigwa <47467035+ekm6@users.noreply.github.com>
2020-01-24 11:47:23 -0500

    Add files via upload

--- docs/proj.js
+++ docs/proj.js
@@ -1,5 +1,6 @@
-const body = d3.select(".wrapper");
-const titleSeq = body.append("div").style("position", "relative");
+const body = d3.select("body");
+const wrapper = d3.select(".wrapper");
+const titleSeq = wrapper.append("div").style("position", "relative");
 
 titleSeq
   .append("h1")
@@ -72,16 +73,206 @@ titleSeq
   .style("opacity", "1");
 
 function leaveTitle() {
-  titleSeq.remove()
-   /* .style("position", "absolute")
+  titleSeq.remove();
+  /* .style("position", "absolute")
     .transition()
     .duration(3000)
     .attr("transform", "translateY(900)"); */
-  body.style('background-color', 'white')
-  body.append("a")
-    .attr("href", "https://docs.google.com/presentation/d/1CH3mna8_6Ee5iGW3BLgbYrqBDriYEOpVKAjy__0UUjY/edit?usp=sharing")
-    .style("font-size", "40px")
-    .html("slides");
-  
-  console.log("https://docs.google.com/presentation/d/1CH3mna8_6Ee5iGW3BLgbYrqBDriYEOpVKAjy__0UUjY/edit?usp=sharing")
+  body.style("background-color", "white")
+  ReadMaleVFemaleData();
 }
+let MaleAdata = [];
+let FemaleAdata = [];
+let MaleVFemaleData= []
+
+ReadMaleVFemaleData = () => {
+  wrapper
+    .append("svg")
+    .attr("id", "MaleVFemaleASvg")
+    .attr("width", 600)
+    .attr("height", 1050);
+  //.style("background-color", "gray");
+
+  d3.csv("../data/Male-vs-Female-Attractiveness.csv")
+    .then(data => {
+      MaleVFemaleData = data;
+    })
+    .then(drawMaleVFemaleData);
+};
+
+
+ReadMaleAdata = () => {
+  wrapper
+    .append("svg")
+    .attr("id", "MaleASvg")
+    .attr("width", 600)
+    .attr("height", 1050);
+
+  d3.csv("../data/Male-Attractiveness-vs-Age.csv")
+    .then(data => {
+      MaleAdata = data;
+    })
+    .then(drawMaleAdata);
+};
+
+ReadFemaleAdata = () => {
+  wrapper
+    .append("svg")
+    .attr("id", "FemaleASvg")
+    .attr("width", 600)
+    .attr("height", 1050);
+  d3.csv("../data/Female-Attractiveness-vs-age.csv")
+    .then(data => {
+      FemaleAdata = data;
+      console.log(FemaleAdata)
+    })
+    .then(drawFemaleAdata);
+};
+
+titleCase = str => {
+  if (str.includes("/")) {
+    let word = str.split("/");
+    return (
+      word[0].charAt(0).toUpperCase() +
+      word[0].substring(1) +
+      " / " +
+      word[1].charAt(0).toUpperCase() +
+      word[1].substring(1)
+    );
+  }
+  return str.charAt(0).toUpperCase() + str.substring(1);
+};
+
+drawMainSection=(svg, data, section, offsetY)=>{
+  
+}
+
+drawSubSection = (svg, data, section, offsetY) => {
+  let scale = d3
+  .scaleLinear()
+  .domain(d3.extent(data.map(d => d[section]).map(Number)))
+  .range([0, 300]);
+
+  d3.select(svg)
+    .selectAll("rect#" + section.replace("/", "-"))
+    .data(data)
+    .enter()
+    .append("rect")
+    .transition()
+    .duration(2000)
+    .attr("x", 300)
+    .attr("y", (d, i) => offsetY + i * 20)
+    .attr("height", 15)
+    .attr("width", d => scale(parseInt(d[section])))
+    .style("fill", "red");
+
+  d3.select(svg)
+    .selectAll("rect#" + section.replace("/", "-"))
+    .data(data)
+    .enter()
+    .append("rect")
+    .transition()
+    .duration(2000)
+    .attr("x", 300)
+    .attr("y", (d, i) => offsetY + i * 20)
+    .attr("height", 15)
+    .attr("width", 1)
+    .style("fill", "black");
+
+  d3.select(svg)
+    .selectAll("text#" + section.replace("/", "-"))
+    .data(data)
+    .enter()
+    .append("text")
+    .transition()
+    .duration(2000)
+    .attr("x", 170)
+    .attr("y", (d, i) => offsetY + i * 20 + 10)
+    .text(d => d.age)
+    .style("color", "black")
+    .style("font-size", "14px");
+
+  d3.select(svg)
+    .append("line")
+    .attr("x1", 165)
+    .attr("y1", offsetY)
+    .attr("x2", 165)
+    .attr("y2", offsetY + 9 * 20)
+    .attr("stroke", "black");
+
+  d3.select(svg)
+    .append("text")
+    .transition()
+    .duration(2000)
+    .attr("x", 0)
+    .attr("y", offsetY + 80)
+    .text(titleCase(section))
+    .style("color", "black")
+    .style("font-size", "14px");
+};
+
+drawMaleVFemaleData=()=>{
+  console.log(MaleVFemaleData)
+  drawMainSection("#MaleVFemaleASvg", MaleVFemaleData, "attractive", 0)
+}
+
+drawMaleAdata = () => {
+  console.log(MaleAdata);
+
+  drawSubSection("#MaleASvg", MaleAdata, "attractive", 0);
+  setTimeout(function() {
+    drawSubSection(
+      "#MaleASvg",
+      MaleAdata,
+      "average/attractive",
+      210
+    );
+  }, 2000);
+  setTimeout(function() {
+    drawSubSection("#MaleASvg", MaleAdata, "average", 420);
+  }, 3500);
+  setTimeout(function() {
+    drawSubSection(
+      "#MaleASvg",
+      MaleAdata,
+      "unattractive/average",
+      630
+    );
+  }, 4500);
+  setTimeout(function() {
+    drawSubSection("#MaleASvg", MaleAdata, "unattractive", 840);
+  }, 5000);
+};
+
+
+drawFemaleAdata = () => {
+
+  drawSubSection("#FemaleASvg", FemaleAdata, "attractive", 0);
+
+  setTimeout(function() {
+    drawSubSection(
+      "#FemaleASvg",
+      FemaleAdata,
+      "average/attractive",
+      210
+    );
+  }, 2000);
+
+  setTimeout(function() {
+    drawSubSection("#FemaleASvg", FemaleAdata, "average", 420);
+  }, 3500);
+
+  setTimeout(function() {
+    drawSubSection(
+      "#FemaleASvg",
+      FemaleAdata,
+      "unattractive/average",
+      630
+    );
+  }, 4500);
+
+  setTimeout(function() {
+    drawSubSection("#FemaleASvg", FemaleAdata, "unattractive", 840);
+  }, 5000);
+};
+
